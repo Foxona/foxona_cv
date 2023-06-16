746b313cda4211e172ce9fc8ef11d63477ebf352
Foxona <sh0n.mishel34@gmail.com>
2023-06-16 16:52:37 +0200

    CV initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Head from "next/head";
+
+const ExperienceSection = () => {
+
+  const myExperiences = [
+    {
+      title: "Fullstack Developer | Bonda",
+      date: "August 2022 - Present",
+      description: [
+        "Developed and maintained front-end and back-end for real-time browser games with websocket connection using MobX, Tailwind CSS, React, NextJS; with Express, Socket.io and MongoDB on the back end.",
+        "Collaborated with a team of designers and product managers to design and implement complex UI features, resulting in a more efficient and visually appealing application.",
+        "Optimized web application performance",
+        "Integrated third-party APIs and services into web applications, such as payment system (Stripe), using AWS S3 for storage, and SES for mailing",
+      ],
+    },
+    {
+      title: "Frontend Developer | Piar OÜ",
+      date: "April 2022 - August 2022",
+      description: [
+        "Developed a website for warehouse management and manufacturing quality control (Material UI)",
+        "Worked together with backend developers to design and implement APIs for the application.",
+        "Worked with project managers to prioritize development tasks, resulting in a more efficient and productive development process.",
+        "Developed simple CI/CD for deploying an application on Vercel",
+      ],
+    },
+    {
+      title: "Frontend Developer | Softway++",
+      date: "February 2021 - April 2022",
+      description: [
+        "Created the frontend portal for teachers, allowing them to create courses with articles, videos, quiz tests, and homework files.",
+        "Developed website section for students, allowing them to study and participate in courses with commenting.",
+        "Collaborated with UX designers to implement user suggestions and improve the user experience of the web application.",
+        "Created custom UI components using React, Ant Design, resulting in a consistent and visually appealing design language across the web application.",
+        "Continuously researched and experimented with new frontend technologies, such as Firebase, Effector, etc.",
+      ],
+    },
+  ];
+
+  return myExperiences.map((experience) => (
+    <div className="mb-5" key={experience.title}>
+      <h4 className="text-lg font-semibold">{experience.title}</h4>
+      <p>{experience.date}</p>
+      <ul className="list-disc pl-5">
+        {experience.description.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+};
 
 export default function Home() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+
+  console.log(currentMonth, currentYear);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gray-100 px-5 py-10 text-black">
+      <Head>
+        <title>Kirill Voronin</title>
+      </Head>
+      <div className="mx-auto max-w-4xl rounded-md bg-white p-10 shadow-md">
+
+        <div className="flex justify-between items-center">
+          <h1 className="mb-5 text-4xl font-bold">Kirill Voronin</h1>
+          <h1 className="mb-5 text-2xl">June 2023, Amsterdam</h1>
         </div>
+
+        <h2 className="text-2xl font-semibold">Front End Developer</h2>
+        <div className="flex items-center gap-4">
+          {/* <a href="tel:+31685230402">+31 685230402</a> */}
+          <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="mailto:sodeepspace@gmail.com">sodeepspace@gmail.com</a>
+          <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/foxona">github.com/foxona</a>
+          <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://t.me/lucawio">t.me/lucawio</a>
+        </div>
+        <h3 className="text-xl font-semibold mt-3">Skills</h3>
+
+        <div>
+          <p>Web Development, JS, TS, React, NextJS</p>
+          <p>Express, MongoDB, Stripe, Websockets</p>
+          <p className="mb-5">HTML5, CSS, Tailwind, Material UI, Ant D</p>
+        </div>
+
+        <h3 className="mb-2 text-xl font-semibold">Experience</h3>
+        <ExperienceSection />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }

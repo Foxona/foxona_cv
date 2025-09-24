787d0b55fd708cd71ba17070d66ea63b68bc0072
Kira <kira@chillchat.me>
2025-09-24 15:32:22 +0200

    Relevant experience added, React package updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,81 +2,6 @@
 import Link from "next/link";
 import { FaGithub, FaFilePdf, FaLinkedinIn } from "react-icons/fa";
 
-const ExperienceSection = () => {
-  const myExperiences = [
-    {
-      title: "Self-Employed, Voronis",
-      href: "/voronis",
-      date: "Jan 2024 - Present",
-      description: [
-        "Developing software solutions for a FinTech startup focusing on gamified investment",
-        "Manufacturing liaison services, including sourcing and procurement",
-      ],
-    },
-    {
-      title: "Fullstack Developer, VeiligWerk",
-      date: "Nov 2023 - Dec 2024",
-      description: [
-        "Released the construction safety management application to clients, leading to improved safety compliance on construction sites",
-        "Migrated a legacy PHP web app to a modern React + NodeJS/Express stack, enhancing usability and maintainability",
-        "Led the team of 3 developers, contributing to team planning, and coordinating efforts across different departments",
-        "Proposed and implemented solutions for project scalability and performance, including the introduction of TypeScript, which reduced defects and accelerated development",
-        "Developed multiple frontend/full-stack modules in collaboration with designers and developers, often taking the lead in guiding their work and ensuring alignment with project goals",
-        "Added Sentry and other debugging tools to improve error monitoring and reduce troubleshooting time",
-        "Built custom development tools to enhance team productivity and streamline workflows"
-      ],
-    },
-    {
-      title: "Fullstack Developer, Bonda",
-      date: "August 2022 - Nov 2023",
-      description: [
-        "Creating and maintaining both front-end and back-end for real-time browser games utilizing Socket.io, MobX (reactive programming), Tailwind, React, NextJS, Express, and MongoDB",
-        "Integrating the backend with business-critical APIs, such as Stripe for payment and trials, Notion, Google Analytics & Auth, and AWS SES, which led to a successful investment round",
-        "Collaborating with a team of designers and product managers to implement UI features, resulting in a more engaging and seamless user experience",
-      ],
-    },
-    {
-      title: "Frontend Developer, Piar OÜ",
-      date: "April 2022 - August 2022",
-      description: [
-        "Building a warehouse management and manufacturing quality control system (Material UI)",
-        "Introducing a more agile process by working closely with the management team",
-        "Reducing time to release by implementing CI/CD and switching to Vercel for deployment",
-      ],
-    },
-    {
-      title: "Frontend Developer, Softway++",
-      date: "February 2021 - April 2022",
-      description: [
-        "Developing an extensive online education platform SPA with features such as courses, articles, videos, quiz tests, and homework files, using React, NextJS and Ant Design",
-        "Collaborating with UI designers to iterate on user feedback and improve the user experience",
-        "Continuously researching ways to simplify code using new frontend technologies such as Firebase, Effector, and more",
-      ],
-    },
-  ];
-
-  return (
-    <>
-      <h3 className="mb-2 p-4 px-2 text-xl font-semibold">Experience</h3>
-      {myExperiences.map((experience) => (
-        <div className="mb-5 px-0" key={experience.title}>
-          <div className="flex flex-col justify-between print:flex-row print:items-center md:flex-row md:items-center">
-            {experience.href ?
-              <Link href={experience.href} className="text-lg font-semibold hover:underline">{experience.title}</Link>
-              : <h4 className="text-lg font-semibold">{experience.title}</h4>}
-            <span className="pr-5 text-gray-500">{experience.date}</span>
-          </div>
-          <ul className="mt-1 list-disc pl-5">
-            {experience.description.map((item, i) => (
-              <li key={i}>{item}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </>
-  );
-};
-
 const styles = {
   link: "font-medium text-blue-600 dark:text-blue-500 hover:underline",
   linkIcon: "hover:text-blue-600 hover:hover:underline",
@@ -96,11 +21,6 @@ const links = [
     link: "https://github.com/foxona",
     icon: <FaGithub className={styles.linkIcon} size={30} />,
   },
-  // {
-  //   name: "telegram",
-  //   link: "https://t.me/lucawio",
-  //   icon: <FaTelegram className={styles.linkIcon} size={30} />,
-  // },
   {
     name: "linkedin",
     link: "https://www.linkedin.com/in/foxona",
@@ -126,15 +46,15 @@ const SkillsSection = () => {
   const skills = [
     {
       tagColor: "bg-secondary",
-      skills: ["React", "NextJS", "SolidJS" "MobX", "Redux", "Tailwind CSS", "Vite",
+      skills: ["TypeScript", "React", "NextJS", "SolidJS", "MobX", "Redux", "Tailwind CSS", "Vite"],
     },
     {
       tagColor: "bg-secondary",
-      skills: ["NodeJS", "Express", "MongoDB", "MySQL", "Websockets", "AWS", "REST API", "Rust", "Stripe", "Bevy"],
+      skills: ["NodeJS", "Express", "AWS", "GCP", "MySQL", "MongoDB", "Websockets", "Stripe"],
     },
     {
       tagColor: "bg-secondary",
-      skills: ["TypeScript", "JavaScript", "Rust"],
+      skills: ["Bash", "Python", "Rust", "Go"],
     },
   ] as const;
   return (
@@ -170,7 +90,7 @@ export default function Home() {
           <div className="flex flex-col">
             <div className="pb-4 pt-2 print:pb-0 print:pt-0 md:pt-0">
               <div className="flex flex-col print:items-end md:items-end">
-                <h1 className="text-lg">October 2024, Amsterdam</h1>
+                <h1 className="text-lg">September 2025, Amsterdam</h1>
                 <a className={`${styles.link} leading-5`} href="mailto:sodeepspace@gmail.com">
                   sodeepspace@gmail.com
                 </a>
@@ -183,9 +103,87 @@ export default function Home() {
           </div>
         </div>
 
+        <div>
+          Engineer based in Amsterdam focused on reliable, secure, and maintainable web systems. I&nbsp;specialize in modern React/Next.js frontends and NodeJS backends, integrating payments
+          and third‑party APIs. Experience shipping real‑time features, migrating legacy stacks, and setting up observability and CI/CD. Comfortable in regulated, process‑oriented
+          environments.
+          {/* I enjoy making DIY electronics and VR games in my spare time. */}
+        </div>
+
         <SkillsSection />
         <ExperienceSection />
       </div>
     </div>
   );
 }
+
+const ExperienceSection = () => {
+  const myExperiences = [
+    {
+      title: "Fullstack Developer, ChillChat",
+      date: "Dec 2024 - Sep 2025",
+      description: [
+        "Developed and released a Web3 multiplayer game using SolidJS, Vite, Tailwind",
+        "Implemented mobile game UI in Rust and Bevy (Lunex, Hui)",
+        "Set up GitLab CI pipelines to deploy containers to Kubernetes and Nomad",
+        "Secured Web Application with Google Identity-Aware Proxy across multiple environments",
+      ],
+    },
+    {
+      title: "Fullstack Developer, VeiligWerk",
+      date: "Nov 2023 - Dec 2024",
+      description: [
+        "Released construction safety management application, improving on‑site compliance",
+        "Migrated a legacy PHP/MySQL codebase to React + Node/ExpressJS + TypeScript, raising developer velocity and maintainability",
+        "Led the team of 3 developers and coordinated efforts across departments",
+        "Introduced TypeScript and Sentry, reducing defects and cutting troubleshooting time",
+        "Built internal developer tools that streamlined repetitive tasks and improved release confidence",
+      ],
+    },
+    {
+      title: "Fullstack Developer, Bonda",
+      date: "August 2022 - Nov 2023",
+      description: [
+        "Created and maintained both front-end and back-end for real-time browser games utilizing Socket.io, MobX (reactive programming), Tailwind, React, NextJS, Express, and MongoDB",
+        "Integrated the backend with business-critical APIs, such as Stripe for payment and trials, Notion, Google Analytics & Auth, and AWS SES, which led to a successful investment round",
+      ],
+    },
+    {
+      title: "Frontend Developer, Piar OÜ",
+      date: "April 2022 - August 2022",
+      description: [
+        "Built a warehouse management and manufacturing quality control system (Material UI)",
+        "Introduced a more agile process by working closely with the management team",
+        "Reduced time to release by implementing CI/CD and switching to Vercel for deployment",
+      ],
+    },
+    {
+      title: "Frontend Developer, Softway++",
+      date: "February 2021 - April 2022",
+      description: [
+        "Developed an extensive online education platform SPA with features such as courses, articles, videos, quiz tests, and homework files, using React, NextJS and Ant Design",
+        "Continuously researched ways to simplify code using new frontend technologies such as Firebase, Effector, and more",
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <h3 className="mb-2 p-4 px-2 text-xl font-semibold">Experience</h3>
+      {myExperiences.map((experience) => (
+        <div className="mb-5 px-0" key={experience.title}>
+          <div className="flex flex-col justify-between print:flex-row print:items-center md:flex-row md:items-center">
+            <h4 className="text-lg font-semibold">{experience.title}</h4>
+
+            <span className="pr-5 text-gray-500">{experience.date}</span>
+          </div>
+          <ul className="mt-1 list-disc pl-5">
+            {experience.description.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
+};
